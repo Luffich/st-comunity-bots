d7058587a5b52aaeeb5e92a5583e8bfa12c65e2d
Luffich <78551121+Luffich@users.noreply.github.com>
2021-07-04 00:14:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -277,6 +277,21 @@ async def info(ctx,member:discord.Member):
     emb.add_field(name='Айди:',value=member.id,inline=False)
     emb.add_field(name='Аккаунт был создан:',value=member.created_at.strftime("%d.%m.%Y %H:%M:%S"),inline=False)
     emb.add_field(name='Самая высокая роль:',value=member.top_role.mention,inline=False)
+
+    if member.status == discord.Status.online:
+        emb.add_field(name='Статус дискорд:', value=':green_circle: Онлайн', inline=False)
+    elif member.status == discord.Status.dnd:
+        emb.add_field(name='Статус дискорд:', value=':no_entry: Не беспокоить', inline=False)
+    elif member.status == discord.Status.offline:
+        emb.add_field(name='Статус дискорд:', value=':black_circle: Нет в сети', inline=False)
+    elif member.status == discord.Status.idle:
+        emb.add_field(name='Статус дискорд:', value=':crescent_moon: Отошол', inline=False)
+ 
+    if member.activity is not None:
+        emb.add_field(name='Кастом статус:', value=member.activity, inline=False)
+    else:
+        emb.add_field(name='Кастом статус:', value='Нету', inline=False)
+
     emb.set_thumbnail(url=member.avatar_url)
     emb.set_footer(text=f"Вызвано: {ctx.message.author}",icon_url=ctx.message.author.avatar_url)
     emb.set_author(name=ctx.message.author,icon_url=ctx.message.author.avatar_url)
